5666c38aa79e6c36dc4150a39e9bac61681ed4fb
jh3n1 <142435558+jh3n1@users.noreply.github.com>
2024-02-27 12:19:38 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+// x é o nome da variável e 1 é o valor atribuído a ela. A palavra-chave var é usada para declarar uma variável em JavaScript.
+var x = 1;
+// mesma coisa da explicação acima, apenas muda os elementos
+var y = 2;
+// if verifica se o valor da variável x é igual ao valor da variável y. Se for verdadeiro, o código dentro das chaves será executado.
+if (x == y) {
+ // A função console.log é usada para imprimir uma mensagem na janela do console do navegador ou terminal. Neste exemplo, a mensagem "Números coincidem" será impressa quando a função for chamada.
+    console.log("Números conferem");
+    // else é usada em conjunto com a declaração if para executar um bloco de código quando a condição da declaração if for falsa.
+} else{
+//    serve para imprimir a mensagem "Números divergem" no console do navegador ou terminal.
+    console.log("Números divergem")
+    // serve para "fechar" o codigo
+}
